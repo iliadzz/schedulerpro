577e252a82b2bd6f0597cb1fb401f5752cfa3a45
iliadzz <brad.grandorff@gmail.com>
2025-08-18 13:01:40 -0600

    V1.1.17 Stable Code. Purge CSS

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,17 +3,25 @@ module.exports = {
   // These are the files PurgeCSS will scan for CSS class usage.
   content: ['public/index.html', 'public/js/**/*.js'],
 
-  // This is the CSS file it will clean up.
-  css: ['public/Style.css'],
+  // This is the list of CSS files it will clean up.
+  // We've added your new files here.
+  css: [
+    'public/Style.css',
+    'public/css/login.css',
+    'public/css/components.css'
+  ],
 
   // This is a crucial list of classes to NEVER remove.
-  // We use regular expressions (the text between /.../) to protect
-  // all Font Awesome classes and our role-based visibility classes.
+  // We use regular expressions to protect Font Awesome and role-based classes.
   safelist: [
     /^fa-/,
     /^fas$/,
     /^far$/,
     /^fab$/,
     /^(admin|manager)-only$/
-  ]
+  ],
+  
+  // This tells PurgeCSS where to put the cleaned files.
+  // It's good practice to specify this directly in the config.
+  output: 'purged_css'
 }
